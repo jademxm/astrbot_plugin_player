a6d7aeb19035ade3a264811c2d06b8d4021974b3
mxm <2503665770@qq.com>
2026-06-30 09:22:13 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import astrbot.api.message_components as Comp
 from astrbot.api.message_components import Video
 import os
 import random
@@ -35,11 +36,13 @@ class MyPlugin(Star):
     @filter.command("player")
     async def player(self, event: AstrMessageEvent):
         """这是一个 player 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        chain = [
+            Comp.At(qq=event.get_sender_id()), # At 消息发送者
+            Comp.Plain("来看这个图："),
+            Comp.Image.fromFileSystem("/Users/mixuanmin/Desktop/搞笑.png"), # 从本地文件目录发送图片
+            Comp.Plain("这是一个图片。")
+        ]
+        yield event.chain_result(chain)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
