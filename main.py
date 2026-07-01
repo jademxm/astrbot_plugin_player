88da0cd956e2ca440b975c1b8faa3424a78073cf
mxm <2503665770@qq.com>
2026-07-01 16:02:16 +0800

    init

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter.tix import IMAGE
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
@@ -7,15 +9,16 @@ import os
 import random
 
 VIDEO_DIR = "/Users/mixuanmin/data/attachments"
+IMAGE_DIR = "/Users/mixuanmin/Desktop"
 
-def search_video(keyword: str) -> str | None:
+def search_video(path:str,keyword: str) -> str | None:
         """
         模糊匹配文件名（包含后缀）
         """
         keyword = keyword.lower()
         matched = []
 
-        for fname in os.listdir(VIDEO_DIR):
+        for fname in os.listdir(path):
             if keyword in fname.lower():
                 matched.append(os.path.join(VIDEO_DIR, fname))
 
@@ -34,13 +37,16 @@ class MyPlugin(Star):
 
     # 注册指令的装饰器。指令名为 player。注册成功后，发送 `/player` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("player")
-    async def player(self, event: AstrMessageEvent):
-        """这是一个 player 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    async def player(self, event: AstrMessageEvent,keyword: str):
+        """用法：/player 图片名""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        if keyword == "":
+            yield event.plain_result("请输入关键字，例如：/video 哈哈")
+            return
         chain = [
             Comp.At(qq=event.get_sender_id()), # At 消息发送者
-            Comp.Plain("来看这个图："),
-            Comp.Image.fromFileSystem("/Users/mixuanmin/Desktop/搞笑.png"), # 从本地文件目录发送图片
-            Comp.Plain("这是一个图片。")
+            # Comp.Plain("来看这个图："),
+            Comp.Image.fromFileSystem(search_video(IMAGE_DIR,keyword)), # 从本地文件目录发送图片
+            # Comp.Plain("这是一个图片。")
         ]
         yield event.chain_result(chain)
 
@@ -54,7 +60,7 @@ class MyPlugin(Star):
             yield event.plain_result("请输入关键字，例如：/video 哈哈")
             return
 
-        path = search_video(keyword)
+        path = search_video(VIDEO_DIR,keyword)
 
         if path == None:
             yield event.plain_result(f"没找到和「{keyword}」相关的视频 😢")
